ad8c25a0a439297debc64a82799e318a40324c02
Raphael Kallweit <117139036+raphaelreal@users.noreply.github.com>
2022-11-09 13:50:18 +0100

    Update steamdb.pas

--- steamdb.pas
+++ steamdb.pas
@@ -1,1 +1,48 @@
-
+_________________________________________________________________________
+steamID:	STEAM_0:1:228003550
+steamID3:	[U:1:456007101]
+steamID64 (Dec):	76561198416272829
+steamID64 (Hex):	11000011b2e1dbd
+customURL	https://steamcommunity.com/id/6_FAKE_6
+profile	https://steamcommunity.com/profiles/76561198416272829
+profile state	Public
+profile created	August 27th, 2017
+name	.fakE
+real name	Jonas
+location	Berlin, ​Berlin​, Germany
+_________________________________________________________________________
+steamID:	STEAM_0:0:586618465
+steamID3:	[U:1:1173236930]
+steamID64 (Dec):	76561199133502658
+steamID64 (Hex):	110000145ee2cc2
+customURL	https://steamcommunity.com/id/raphaelreal
+profile	https://steamcommunity.com/profiles/76561199133502658
+profile state	Public
+profile created	January 24th, 2021
+name	󠀡󠀡
+real name	Not set
+location	Not set
+_________________________________________________________________________
+steamID:	STEAM_0:0:149145143
+steamID3:	[U:1:298290286]
+steamID64 (Dec):	76561198258556014
+steamID64 (Hex):	110000111c78c6e
+customURL	https://steamcommunity.com/id/SxpremeDaanii
+profile	https://steamcommunity.com/profiles/76561198258556014
+profile state	Private
+profile created	November 3rd, 2015
+name	Sxpreme.
+real name	Not set
+location	Germany
+_________________________________________________________________________
+steamID:	STEAM_0:0:429615948
+steamID3:	[U:1:859231896]
+steamID64 (Dec):	76561198819497624
+steamID64 (Hex):	11000013336d698
+profile	https://steamcommunity.com/profiles/76561198819497624
+profile state	Private
+profile created	Private
+name	ニキタ
+real name	Not set
+location	Not set
+_________________________________________________________________________
